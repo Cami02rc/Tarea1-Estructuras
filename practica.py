34523819e765eeab33fe7a5189c8d15343efc6e9
unknown <camilaurodriguezc@gmail.com>
2023-09-14 20:31:20 -0600

    Tarea 1 Datos

---
+++ practica.py
@@ -0,0 +1,20 @@
+print ( "Hola Mundo" )
+
+print (7+5-6)
+
+print (9+7*8-36/5)
+
+print (5*2+3)
+print (5*(2+3))
+print 
+#se solicita al usuario la edad 
+print ("Ingrese su edad")
+#Se guarda como una cadena de texto
+ingreso =input()
+#Se pasa la cadena a una variable
+edad=int(ingreso)
+
+edadSuma =5
+proximaEdad= edad+edadSuma
+print("Su edad es ", edad)
+print("Dentro de 5 años su edad será", proximaEdad)
